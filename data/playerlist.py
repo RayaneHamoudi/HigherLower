abb8957e1adc978a66e1b44e0cfc9ebd56d6b8da
Rafay Syed <65063591+eskitete@users.noreply.github.com>
2025-01-09 04:13:39 -0600

    draft years

--- data/playerlist.py
+++ data/playerlist.py
@@ -8,7 +8,9 @@ mvp = pd.read_csv('mvp.csv')
 roty = pd.read_csv('roty.csv')
 fmvp = pd.read_csv('fmvp.csv')
 sixman = pd.read_csv('sixman.csv')
+draftYear = pd.read_csv('draftyear.csv')
 player_data = dict(zip(allstar["Player"], allstar["times"]))
+player_datadraft = dict(zip(draftYear["Name"], draftYear["DraftYear"]))
 #list all player names and the number of times they have won each award
 def player_list():
     allstar_list = pd.Series(player_data)
@@ -17,13 +19,14 @@ def player_list():
     mvp_list = mvp['Player'].value_counts()
     roty_list = roty['Player'].value_counts()
     fmvp_list = fmvp['Player'].value_counts()
-    sixman_list = sixman['Player'].value_counts()   
-    
-    player_list = pd.concat([allstar_list, dpoy_list, mvp_list, roty_list, fmvp_list, sixman_list, allnba_list], axis=1)
+    sixman_list = sixman['Player'].value_counts() 
+    draftyear_list = pd.Series(player_datadraft)
+
+    player_list = pd.concat([allstar_list, dpoy_list, mvp_list, roty_list, fmvp_list, sixman_list, allnba_list,draftyear_list], axis=1)
     #fill nan with 0
     player_list = player_list.fillna(0)
 
-    player_list.columns = ['All-Star', 'DPOY', 'MVP', 'ROTY', 'FMVP', 'Six-Man',"All-NBA"]
+    player_list.columns = ['All-Star', 'DPOY', 'MVP', 'ROTY', 'FMVP', 'Six-Man',"All-NBA","Draft-Year"]
     #make json
     player_list.to_csv('player_list.csv')
     
